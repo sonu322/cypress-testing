ed380f1b3f5d5cb310486418906541029a1ec672
Deepak Jangid <deepak.jangid@optimizory.com>
2019-06-17 12:33:44 +0530

    Added frontend license support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Toolbar from "./components/Toolbar";
 import IssueTree from "./components/IssueTree";
+import URLSearchParams from "@ungap/url-search-params";
+import styled from "styled-components";
+import { colors } from "@atlaskit/theme";
+
+const NoLicense = styled.div`
+  color: ${colors.R500};
+`;
 
 class Main extends React.Component {
   constructor(props) {
@@ -32,6 +39,15 @@ class Main extends React.Component {
   }
 }
 
+const searcher = new URLSearchParams(location.search);
 const App = document.getElementById("app");
 
-ReactDOM.render(<Main />, App);
+const LicenceContainer = () => {
+  return <NoLicense>Error: you don't have valid license</NoLicense>;
+};
+
+if (searcher.has("lic") && "none" === searcher.get("lic")) {
+  ReactDOM.render(<LicenceContainer />, App);
+} else {
+  ReactDOM.render(<Main />, App);
+}
