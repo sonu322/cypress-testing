89e3b9f633928b9a7cb241e7ee6bdcfe221d8e9f
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2022-09-21 12:38:04 +0530

    Removed external dependency on jq command, added README content for local plugin development

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ** Plugin for Jira Cloud **
 
-# Requires ngrok, jq, yarn, node.
+# Requires ngrok, yarn, node.
+
+## For local plugin development
+ - First start the server to serve the plugin descriptor and other static files using the command below:
+   - `yarn start` 
+ - In another tab, run the below command to open the ngrok tunnel and update the plugin descriptor file:
+   - `yarn serve`
+   - The above command will display the app descriptor URL, copy this URL and open it in the browser first because `ngrok` display a warning before serving any content for the first time. In the warning page, click on `Visit Page` link to remove the warning from subsequent visits.
+ - Now copy the app descriptor URL from the above step and install the plugin in Jira UPM using that URL.
--- serve.js
+++ serve.js
@@ -1,17 +1,15 @@
 const ngrok = require("ngrok");
-const shelljs = require("shelljs");
+const jq = require('node-jq');
+const fs = require('fs');
 
 (async function() {
-  const url = await ngrok.connect(1234);
-  console.info(`${url}/atlassian-connect.json`);
-  const commands = {
-    "create-temporary-descriptor": `jq '.baseUrl = "${url}" | .links.self = "${url}/atlassian-connect.json"' assets/original-atlassian-connect.json > assets/atlassian-connect.json`,
-    //"move-file": `mv temporary-atlassian-connect.json assets/atlassian-connect.json`
-    //"open-ngrok-management": `/usr/bin/open http://127.0.0.1:4040`
-  };
-
-  Object.entries(commands).forEach(([name, command]) => {
-    console.info(`Executing command to: ${name.replace(/-/g, " ")}`);
-    shelljs.exec(command);
-  });
+  try {
+    const url = await ngrok.connect(1234);
+    console.log("App Descriptor URL: ");
+    console.info(`${url}/atlassian-connect.json`);
+    let file = await jq.run(`.baseUrl = "${url}" | .links.self = "${url}/atlassian-connect.json"`, 'assets/original-atlassian-connect.json');
+    fs.writeFileSync('assets/atlassian-connect.json', file);
+  } catch(err){
+    console.error(err);
+  }
 })();
