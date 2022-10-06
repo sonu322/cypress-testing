50ab1689c4b5ae931f7518b6afa3e06df7981e54
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-10-06 14:29:57 +0530

    add button group

--- src/components/IssueTreeModule/Toolbar/Toolbar.jsx
+++ src/components/IssueTreeModule/Toolbar/Toolbar.jsx
@@ -2,6 +2,7 @@ import Page, { Grid, GridColumn } from "@atlaskit/page";
 import React, { useState } from "react";
 import { IssueTypeAPI, LinkTypeAPI, PriorityAPI } from "../../api";
 import { Dropdown } from "./Dropdown";
+import Button, { ButtonGroup } from "@atlaskit/button";
 export const Toolbar = ({
   options,
   filter,
@@ -12,30 +13,32 @@ export const Toolbar = ({
     <div>
       <Grid spacing="compact">
         <GridColumn medium={10}>
-          <Dropdown
-            options={options}
-            filter={filter}
-            keyName={"issueTypes"}
-            updateKeyOptions={updateKeyOptions}
-            updateFilteredKeyOptions={updateFilteredKeyOptions}
-            api={IssueTypeAPI}
-          />
-          <Dropdown
-            options={options}
-            filter={filter}
-            keyName={"linkTypes"}
-            updateKeyOptions={updateKeyOptions}
-            updateFilteredKeyOptions={updateFilteredKeyOptions}
-            api={LinkTypeAPI}
-          />
-          <Dropdown
-            options={options}
-            filter={filter}
-            keyName={"priorities"}
-            updateKeyOptions={updateKeyOptions}
-            updateFilteredKeyOptions={updateFilteredKeyOptions}
-            api={PriorityAPI}
-          />
+          <ButtonGroup>
+            <Dropdown
+              options={options}
+              filter={filter}
+              keyName={"issueTypes"}
+              updateKeyOptions={updateKeyOptions}
+              updateFilteredKeyOptions={updateFilteredKeyOptions}
+              api={IssueTypeAPI}
+            />
+            <Dropdown
+              options={options}
+              filter={filter}
+              keyName={"linkTypes"}
+              updateKeyOptions={updateKeyOptions}
+              updateFilteredKeyOptions={updateFilteredKeyOptions}
+              api={LinkTypeAPI}
+            />
+            <Dropdown
+              options={options}
+              filter={filter}
+              keyName={"priorities"}
+              updateKeyOptions={updateKeyOptions}
+              updateFilteredKeyOptions={updateFilteredKeyOptions}
+              api={PriorityAPI}
+            />
+          </ButtonGroup>
           {/* <LinkTypeDropdown
               filter={(data) => this.updateLinkTypeFilter(data)}
             />
