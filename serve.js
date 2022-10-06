ac874362ababe52451df06da5121bfb89c78c3ee
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-10-06 10:14:45 +0530

    use localtunnel

--- serve.js
+++ serve.js
@@ -1,10 +1,13 @@
 const ngrok = require("ngrok");
+const localtunnel = require("localtunnel")
 const jq = require('node-jq');
 const fs = require('fs');
 
 (async function() {
   try {
-    const url = await ngrok.connect(1234);
+    // const url = await ngrok.connect(1234);
+    const tunnel = await localtunnel({ port: 1234 })
+    let url = tunnel.url
     console.log("App Descriptor URL: ");
     console.info(`${url}/atlassian-connect.json`);
     let file = await jq.run(`.baseUrl = "${url}" | .links.self = "${url}/atlassian-connect.json"`, 'assets/original-atlassian-connect.json');
