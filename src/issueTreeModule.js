b1287b01c1c0b1ca03d40acbb46d3ec422adb23b
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2022-10-07 13:18:56 +0530

    add eslint

--- src/issueTreeModule.js
+++ src/issueTreeModule.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import URLSearchParams from "@ungap/url-search-params";
 import LicenseContainer from "./components/LicenseContainer";
 import { IssueTreeModule } from "./components/IssueTreeModule/IssueTreeModule";
+// eslint-disable-next-line no-undef
 const searcher = new URLSearchParams(location.search);
+// eslint-disable-next-line no-undef
 const App = document.getElementById("app");
 
 if (searcher.has("lic") && "none" === searcher.get("lic")) {
