f6c04b1e094c7faaa6ea04c31e837c2fb087fa5b
gmahima <mahima@optimizory.com>
2022-10-25 16:17:48 +0530

    remove unused code

--- src/components/TracebilityReportModule/Toolbar.tsx
+++ src/components/TracebilityReportModule/Toolbar.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import styled from "styled-components";
 import { colors } from "@atlaskit/theme";
-import Button, { ButtonGroup } from "@atlaskit/button";
-import { DropdownSingleSelect } from "../DropdownSingleSelect";
 import { APIContext } from "../../context/api";
 import { Filter } from "../../types/api";
 import { JQLSelectDropdown } from "../JQLSelectDropdown";
@@ -16,29 +14,6 @@ const MainBar = styled.div`
 `;
 
 export const Toolbar = () => {
-  const api = useContext(APIContext);
-  const [filters, setFilters] = useState<Filter[]>([]);
-  const [selectedFilterId, setSelectedFilterId] = useState<String>();
-  useEffect(() => {
-    const fetchFilters = async () => {
-      try {
-        let response = await api.getFilters();
-        setFilters(response);
-        return response;
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchFilters();
-  }, []);
-
-  const selectedOption = filters.find(
-    (filter) => filter.id == selectedFilterId
-  );
-  const fetchFilteredIssues = () => {
-    // const jql = "filter=" + selectedOption?.id;
-    //TODO: fetch filtered ids
-  };
   return (
     <MainBar>
       <JQLSelectDropdown />
