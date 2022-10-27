32e681ab5e33f5563f658e41c6540a004bf412b1
gmahima <mahima@optimizory.com>
2022-10-27 19:56:56 +0530

    fix height

--- src/components/TracebilityReportModule/TracebilityReportModule.tsx
+++ src/components/TracebilityReportModule/TracebilityReportModule.tsx
@@ -9,7 +9,7 @@ import { IssueField } from "../../types/api";
 import { Main } from "./Main";
 const FullWidthContainer = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   background-color: pink;
 `;
 const fixedFieldNames = [
