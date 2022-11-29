bb361201808bd7f79e597b230c59d82b71f3c753
gmahima <mahima@optimizory.com>
2022-11-29 10:04:01 +0530

    add parent and subtasks

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ const linksRNG = Util.getRNG("linksRNG");
 const linkFinderRNG = Util.getRNG("linkFinderRNG");
 const linkTypesRNG = Util.getRNG("linkTypesRNG");
 const versionsRNG = Util.getRNG("versionsRNG");
-
+const parentIssueNumberRNG = Util.getRNG("no-of-parent-issues");
 const module = {
   async generateProjects(): Promise<any[]> {
     // {{baseURL}}/rest/api/3/myself
@@ -32,39 +32,68 @@ const module = {
         "sample description 400 700 random",
         myself.accountId,
         "com.pyxis.greenhopper.jira:gh-simplified-agility-kanban",
-        "no-epic-subtask-test-3",
-        "NEST3"
+        "subtask-parent-test-1",
+        "SPT1"
       )
     );
     console.log("in gen project");
     console.log(projects);
-    projects.push(
-      await api.createProject(
-        "sample description",
-        myself.accountId,
-        "com.pyxis.greenhopper.jira:gh-simplified-scrum-classic",
-        "no-epic-subtask-test-4",
-        "NEST4"
-      )
-    ); // classic project
+    // projects.push(
+    //   await api.createProject(
+    //     "sample description",
+    //     myself.accountId,
+    //     "com.pyxis.greenhopper.jira:gh-simplified-scrum-classic",
+    //     "new-subtask-test-10",
+    //     "NST10"
+    //   )
+    // ); // classic project
     return projects;
   },
-
+  async generateSubtasks(project, noOfIssues, subtaskFieldName, parentIssues) {
+    const parentIssueKeys = parentIssues.map((parentIssue) => parentIssue.key);
+    const childIssues = await api.createSubtasksInBulk(
+      project,
+      noOfIssues,
+      subtaskFieldName,
+      parentIssueKeys
+    );
+    return childIssues;
+  },
   async generateIssues(projects: any, noOfIssues: number): Promise<any[]> {
     console.log("in gen issues");
     console.log(projects);
     let issues: any[] = [];
     for (let i = 0; i < projects.length; i++) {
       const issueTypeNames = await api.getProjectIssueTypeNames(projects[i]);
-      const resultantIssues = await api.createIssuesInBulk(
+      const parentIssueTypeNames = issueTypeNames.filter(
+        (type) => !type.includes("Sub")
+      );
+      // parents
+      const noOfParents = Util.getPositiveRandomNumber(
+        parentIssueNumberRNG,
+        // noOfIssues
+        5
+      );
+      const parentIssues = await api.createIssuesInBulk(
         projects[i],
-        noOfIssues,
-        issueTypeNames
+        noOfParents,
+        parentIssueTypeNames
       );
       console.log("RESULTATNT ISSSUES");
-      console.log(resultantIssues);
-      if (resultantIssues.length > 0) {
-        issues = issues.concat(resultantIssues);
+      console.log(parentIssues);
+      if (parentIssues.length > 0) {
+        issues = issues.concat(parentIssues);
+      }
+      // add subtasks to parents
+      const subtaskField = issueTypeNames.find((type) => type.includes("Sub"));
+      const childIssues = await module.generateSubtasks(
+        projects[i],
+        noOfIssues,
+        subtaskField,
+        parentIssues
+      );
+      if (childIssues.length > 0) {
+        issues = issues.concat(childIssues);
       }
     }
     console.log(
@@ -87,7 +116,7 @@ const module = {
       const noOfLinks = Util.getPositiveRandomNumber(linksRNG, maxLinks + 1);
       console.log("NO OF LINKS", noOfLinks);
       for (let j = 0; j < noOfLinks; j++) {
-        const issueIndex = Util.getPositiveRandomNumber(
+        let issueIndex = Util.getPositiveRandomNumber(
           linkFinderRNG,
           issues.length
         );
