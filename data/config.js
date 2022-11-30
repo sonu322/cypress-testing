2a0f600c41f31b4ad349a5f5682dd1f059c1f88d
gmahima <mahima@optimizory.com>
2022-11-30 11:04:43 +0530

    remove js files and config file

--- data/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    baseURL: "https://mahima-optimizory.atlassian.net",
-    username: "mahima@optimizory.com",
-    password: "L1U0elpThYq8XODDWaeqCE4D",
-    noOfRecords: 20
-};
--- data/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  baseURL: "base url. ex: https://xyz.atlassian.net",
-  username: "username: ex: xyz@abc.com",
-  password: "api token from atlassian",
-  noOfRecords: 20,
-};
