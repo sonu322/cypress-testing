a79fa7e9b5df27f9f7e4a25e0772af8da1fc1ea3
gmahima <mahima@optimizory.com>
2022-12-01 20:41:25 +0530

    add config file

---
+++ data/config.ts
@@ -0,0 +1,6 @@
+export default {
+  baseURL: "base url. ex: https://bob.atlassian.net/rest/api/3",
+  username: "username. ex:bob@bingo.com",
+  password: "atlassin api token",
+  noOfRecords: 100,
+};
