c5fa875dd96513e1fce3a994f62670fd1d62fa3d
gmahima <mahima@optimizory.com>
2022-12-07 12:19:27 +0530

    change back config

--- data/config.ts
+++ data/config.ts
@@ -1,7 +1,7 @@
 export default {
-  baseURL: "https://mahima-optimizory.atlassian.net/rest/api/3",
-  username: "mahima@optimizory.com",
-  password: "hw6wqf65fMtxY9fzlL6W578A",
+  baseURL: "base url. ex: https://bob.atlassian.net/rest/api/3",
+  username: "username. ex:bob@bingo.com",
+  password: "atlassian api token",
   noOfRecords: 100,
   maxLinks: 10,
   maxProjectVersions: 5,
