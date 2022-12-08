799bc475add26ac7e34fb6452049a358670769f1
gmahima <mahima@optimizory.com>
2022-12-08 18:48:26 +0530

    add missing properties and fix names

--- i18n.ts
+++ i18n.ts
@@ -48,8 +48,8 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: "en-US",
-    fallbackLng: "en",
+    lng: "de-DE", // remove before commiting
+    // fallbackLng: "en-US", // uncomment before commiting
     debug: true,
 
     interpolation: {
