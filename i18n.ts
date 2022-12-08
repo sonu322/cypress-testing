66bc7dc316d34ac32874930f29779d02b2a167fd
gmahima <mahima@optimizory.com>
2022-12-08 10:35:01 +0530

    add german file full

--- i18n.ts
+++ i18n.ts
@@ -49,8 +49,8 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: "zh-CN",
-    fallbackLng: "zh-CN",
+    lng: "de-DE",
+    fallbackLng: "de-DE",
     debug: true,
 
     interpolation: {
