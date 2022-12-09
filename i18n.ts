e18bdb3a128939f407d55c589338496361f41139
gmahima <mahima@optimizory.com>
2022-12-09 14:18:37 +0530

    add france

--- i18n.ts
+++ i18n.ts
@@ -48,7 +48,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: "es-ES", // remove before commiting
+    lng: "fr-FR", // remove before commiting
     // fallbackLng: "en-US", // uncomment before commiting
     debug: true,
 
