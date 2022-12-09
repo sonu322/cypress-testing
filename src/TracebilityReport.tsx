9069994d6bfb749ddbeb6ba3f153ad096b28e674
gmahima <mahima@optimizory.com>
2022-12-09 16:27:54 +0530

    change in german'

--- src/TracebilityReport.tsx
+++ src/TracebilityReport.tsx
@@ -1,15 +1,19 @@
 import React, { useContext, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import LicenseContainer from "./components/common/LicenseContainer";
 import { TracebilityReportModule } from "./components/TracebilityReportModule/TracebilityReportModule";
 import { APIContext } from "./context/api";
 
 const TracebilityReport = () => {
+  const { i18n } = useTranslation();
   const api = useContext(APIContext);
   useEffect(() => {
     const handleLocale = async () => {
       const locale = await api.getLocale();
+      console.log("LOCALe");
+      console.log(locale);
       if (locale !== i18n.language) {
-        console.log(i18n.language, locale);
+        console.log("i18", i18n.language, "locale", locale);
         await i18n.changeLanguage(locale);
       }
     };
