70668d3de112204df708165bde56b291fda1d178
gmahima <mahima@optimizory.com>
2022-12-13 16:29:49 +0530

    modfiy jql

--- src/impl/Cloud.ts
+++ src/impl/Cloud.ts
@@ -484,7 +484,7 @@ export default class APIImpl implements LXPAPI {
     console.log(`"Epic Link" = ${issue.issueKey}`);
     const childIssuesData = await this.searchIssues(
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      `parent = ${issue.issueKey}`,
+      `parent = ${issue.issueKey} OR "Epic Link" = ${issue.issueKey}`,
       fields
     );
     console.log("childissues", childIssuesData);
