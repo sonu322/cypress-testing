15a109d6053dbbc3a17e945bf14241f483649585
Anamika Soni <anamika@optimizory.com>
2022-12-14 11:55:23 +0530

    Issue Limit Dropdown.

---
+++ data/config.ts
@@ -0,0 +1,8 @@
+export default {
+  baseURL: "https://anamika-optimizory.atlassian.net/rest/api/3",
+  username: "anamika@optimizory.com",
+  password: "3wxIXVdk9oha0qiIjnK68BB5",
+  noOfRecords: 100,
+  maxLinks: 10,
+  maxProjectVersions: 5,
+};
