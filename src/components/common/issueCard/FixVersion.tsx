8a4eedb31ad76ec40534ac280ad3001d1ab26773
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2022-12-26 11:44:03 +0530

    fix versions wrap issue fixed

--- src/components/common/issueCard/FixVersion.tsx
+++ src/components/common/issueCard/FixVersion.tsx
@@ -1,34 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 import { IssueVersion } from "../../../types/api";
 import Lozenge from "@atlaskit/lozenge";
 import { TooltipContainer } from "../TooltipContainer";
-import { version } from "react-dom";
 
-interface Props { 
+interface Props {
   versionInfo: IssueVersion[];
 }
 
+const Container = styled.div`
+  white-space: nowrap;
+`;
+
 export const FixVersion = ({ versionInfo }: Props): JSX.Element => {
- const length = versionInfo.length;
-  let totalversions, num;
-  if(length > 1){
+  const length = versionInfo.length;
+  let totalversions = "";
+  let num;
+  if (length > 0) {
     totalversions = versionInfo[0]?.name;
-    num = " +" + (versionInfo.length - 1);
+    if (length > 1) {
+      num = ` +${versionInfo.length - 1}`;
+    }
   }
-  else {
-    totalversions = versionInfo[0]?.name;
-  }
-let versionNames = versionInfo.map((versionInfo) => {
+  const versionNames = versionInfo.map((versionInfo) => {
     return versionInfo.name;
-});
-const tooltipContent = versionNames.join(", ");
-    return (
-      <TooltipContainer content={tooltipContent} position="bottom">
+  });
+  const tooltipContent = versionNames.join(", ");
+  return (
+    <TooltipContainer content={tooltipContent} position="bottom">
+      <Container>
         <Lozenge>
-          {totalversions}  
+          {totalversions}
         </Lozenge>
         {num}
-      </TooltipContainer>
-    );
+      </Container>
+    </TooltipContainer>
+  );
 };
 
