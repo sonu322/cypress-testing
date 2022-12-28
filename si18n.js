62a94e5fb9cb88ed5ad1a40727d6e3bb51d3354e
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2022-12-28 17:52:04 +0530

    added script to generate i18n files for server module

---
+++ si18n.js
@@ -0,0 +1,43 @@
+const jsonToProps = require("json-to-properties/src/scripts/processor");
+const native2ascii = require("node-native2ascii");
+const fs = require("fs");
+const path = require("path");
+
+const baseDist = "../lxp-server/src/main/resources/com/otpl/jira/plugins/generated";
+const nonAsciiDist = baseDist + "/non-ascii";
+
+// remove the existing `generated` dir
+fs.rmSync(baseDist, {recursive: true, force: true});
+
+// create new generated dir
+fs.mkdirSync(baseDist);
+fs.mkdirSync(nonAsciiDist);
+
+// convert json to properties file
+jsonToProps.process({config: {
+  src: "./assets/i18n/0.1",
+  dist: nonAsciiDist
+}}); // this method is not synchronous, so we used setTimeout below.
+
+const files = [
+  "de_DE",
+  "en_US",
+  "es_ES",
+  "fr_FR",
+  "zh_CN"
+];
+async function convert2ascii(){
+  for(const file of files){
+    const input = fs.readFileSync(path.resolve(nonAsciiDist + "/" + file + ".properties"), "utf8");
+    const output = native2ascii(input);
+    let outfile = baseDist + "/lxp_" + file + ".properties";
+    if(file === "en_US"){
+      outfile = baseDist + "/lxp.properties";
+    }
+    fs.writeFileSync(path.resolve(outfile), output);
+  }
+}
+
+setTimeout(function(){
+  convert2ascii();
+}, 3000);
