f91d3047c2ab60a09fb045bc4a5e3025bfa3fb86
gmahima <mahima@optimizory.com>
2023-04-03 21:06:53 +0530

    add config form

--- src/components/DashboardGadget/Configuration.tsx
+++ src/components/DashboardGadget/Configuration.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Config, getConfig } from "@atlaskit/util-service-support";
 import ReactDOM from "react-dom";
+import { GadgetConfigurationForm } from "../common/GadgetConfigForm";
 const Configure = () => {
-  // TODO: Define configuration page
   const config = getConfig();
-  console.log("config", config);
+  console.log("config", config); // TODO: this does not work
   return (
     <div>
       <h1>My Gadget Configuration</h1>
-      {/* TODO: Add configuration UI */}
+      <GadgetConfigurationForm
+        onSave={(config) => {
+          console.log("saved config", config);
+        }}
+      />
     </div>
   );
 };
-console.log("config script called");
 const App = document.getElementById("app");
 ReactDOM.render(<Configure />, App);
