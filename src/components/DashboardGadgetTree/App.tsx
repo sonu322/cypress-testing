3079905aa71b2be4fcaca2f37952c83a40b893c3
gmahima <mahima@optimizory.com>
2023-04-10 14:54:05 +0530

    add config form

--- src/components/DashboardGadgetTree/App.tsx
+++ src/components/DashboardGadgetTree/App.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
-import { DEFAULT_GADGET_HEIGHT } from "../../constants/tree";
+import {
+  DEFAULT_GADGET_HEIGHT,
+  DEFAULT_GADGET_TITLE,
+} from "../../constants/tree";
 import { APIContext } from "../../context/api";
 import { TreeGadgetConfig } from "../../types/app";
 import { Gadget } from "./Gadget";
@@ -17,12 +20,9 @@ const Container = styled.div<ContainerProps>`
 `;
 
 const DashboardGadget: React.FC = () => {
-  const [isConfiguring, setIsConfiguring] = useState(false);
-  const [config, setConfig] = useState<TreeGadgetConfig>({
-    title: "",
-    issueKey: "",
-    height: DEFAULT_GADGET_HEIGHT,
-  });
+  const [isConfiguring, setIsConfiguring] = useState(true);
+  const [config, setConfig] = useState<TreeGadgetConfig>();
+
   const [dashboardId, setDashboardId] = useState();
   const [dashboardItemId, setDashboardItemId] = useState();
   const openConfigureScreen = (): void => {
@@ -37,7 +37,9 @@ const DashboardGadget: React.FC = () => {
   const api = new APIImpl(jiraCloud);
   useEffect(() => {
     // Set the height of your gadget using AP.resize
-    AP.resize("100%", config.height);
+    if (config?.height !== undefined) {
+      AP.resize("100%", config.height);
+    }
   }, [config]);
 
   useEffect(() => {
@@ -51,15 +53,71 @@ const DashboardGadget: React.FC = () => {
     if (dashboardItemId !== undefined) {
       setDashboardItemId(dashboardItemId);
     }
+    if (dashboardId !== undefined && dashboardItemId !== undefined) {
+      // AP.request({
+      //   url: `/rest/api/3/dashboard/${dashboardId}/items/${dashboardItemId}/properties/config`,
+      //   success: (response) => {
+      // const data = JSON.parse(response);
+      // console.log("response", data.value);
+      // setConfig(data.value); // last saved value
+      // setIsConfiguring(false);
+      //   },
+      //   failure: () => {
+      //     console.log(
+      //       "failed to get config !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      //     );
+      // setConfig({
+      //   title: "",
+      //   issueKey: "",
+      //   height: DEFAULT_GADGET_HEIGHT,
+      // });
+      // setIsConfiguring(true);
+      //   },
+      // });
+      AP.request({
+        url: `/rest/api/3/dashboard/${dashboardId}/items/${dashboardItemId}/properties/config`,
+      })
+        .then((response) => {
+          const data = JSON.parse(response);
+          console.log("response", data.value);
+          setConfig(data.value); // last saved value
+          setIsConfiguring(false);
+        })
+        .catch((e) => {
+          console.log("FROM CATCH!!!!!!!!");
+          console.log(e);
+          setConfig({
+            title: DEFAULT_GADGET_TITLE,
+            issueKey: "",
+            height: DEFAULT_GADGET_HEIGHT,
+          });
+          setIsConfiguring(true);
+        });
+    }
   }, []);
 
-  if (dashboardId !== undefined && dashboardItemId !== undefined) {
+  AP.require(["jira"], function (jira) {
+    jira.DashboardItem.onDashboardItemEdit(function () {
+      console.log("edit called");
+      // render dashboard item configuration now
+      setIsConfiguring(true);
+    });
+  });
+
+  if (
+    dashboardId !== undefined &&
+    dashboardItemId !== undefined &&
+    config !== undefined
+  ) {
     return (
       <APIContext.Provider value={api}>
-        <Container height={config.height}>
-          <h1>{config.title?.length > 0 ? config.title : "LXP"}</h1>
+        <Container height={config?.height ?? DEFAULT_GADGET_HEIGHT}>
+          <h1>
+            {config?.title?.length > 0 ? config.title : DEFAULT_GADGET_TITLE}
+          </h1>
           {isConfiguring ? (
             <GadgetConfigurationForm
+              savedConfig={config}
               dashboardId={dashboardId}
               dashboardItemId={dashboardItemId}
               onSave={handleSaveConfig}
