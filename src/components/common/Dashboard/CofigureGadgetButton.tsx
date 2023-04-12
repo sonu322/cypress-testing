f6831a99c6eec166a8e8073f538ffb722c89f44e
gmahima <mahima@optimizory.com>
2023-04-13 02:43:14 +0530

    add translations 0.5

--- src/components/common/Dashboard/CofigureGadgetButton.tsx
+++ src/components/common/Dashboard/CofigureGadgetButton.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import Button from "@atlaskit/button";
 import { TooltipContainer } from "../TooltipContainer";
 import SettingsIcon from "@atlaskit/icon/glyph/settings";
+import { useTranslation } from "react-i18next";
 interface Props {
   handleClick: () => void;
 }
 
 export const ConfigureGadgetButton: React.FC<Props> = ({ handleClick }) => {
+  const { t } = useTranslation();
+  const configureGadgetLabel = t(
+    "otpl.lxp.gadget.configure-button.description"
+  );
   return (
     <TooltipContainer content={"Configure Gadget"}>
       <Button
@@ -14,7 +19,7 @@ export const ConfigureGadgetButton: React.FC<Props> = ({ handleClick }) => {
         onClick={handleClick}
         iconBefore={<SettingsIcon label="Configure Gadget" />}
       >
-        Configure
+        {configureGadgetLabel}
       </Button>
     </TooltipContainer>
   );
