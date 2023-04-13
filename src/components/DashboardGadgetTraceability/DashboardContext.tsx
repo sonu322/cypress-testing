bf36c183e9d1a86fe53a6ba4bce32ba066f5723a
gmahima <mahima@optimizory.com>
2023-04-13 06:26:24 +0530

    add traceability reports

---
+++ src/components/DashboardGadgetTraceability/DashboardContext.tsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { TreeGadgetConfig } from "../../types/app";
+
+export const DashboardContext = createContext<{
+  dashboardId: string;
+  dashboardItemId: string;
+  config: TreeGadgetConfig;
+  updateConfig: (config: TreeGadgetConfig) => void;
+  isConfiguring: boolean;
+  updateIsConfiguring: (isConfiguring: boolean) => void;
+}>({
+  dashboardId: undefined,
+  dashboardItemId: undefined,
+  config: undefined,
+  updateConfig: undefined,
+  isConfiguring: true,
+  updateIsConfiguring: undefined,
+});
