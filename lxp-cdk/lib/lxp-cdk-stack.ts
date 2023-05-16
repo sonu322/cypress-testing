2f069f4938eaf8c9a2b37bbdc7378bda00166c96
pulkit <pulkit@optimizory.com>
2023-05-16 19:51:37 +0530

    updated readme file

--- lxp-cdk/lib/lxp-cdk-stack.ts
+++ lxp-cdk/lib/lxp-cdk-stack.ts
@@ -11,15 +11,8 @@ import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as fs from 'fs';
 import * as path from 'path';
 export const Envjson = envParams["dev"];
-//const buildDirectory = path.resolve('C:/Users/pulkit/Desktop/lxp-c');
-// return the path where this file is located
-const buildDirectoryBase = path.basename(__dirname);
 const buildDirectory = path.join(__dirname, 'builds');
-
-
 console.log(buildDirectory);
-
-// select builds driectory
 
 
 export class LxpCdkStack extends cdk.Stack {
@@ -47,9 +40,11 @@ export class LxpCdkStack extends cdk.Stack {
       accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
       // set puclib access to true for testing
     });
+    // origin access identity
     const originAccessIdentity = new cf.OriginAccessIdentity(this, 'LxpOriginAccessIdentity', {
       comment: 'LxpOriginAccessIdentity',
     });
+    // read the directory and deploy the build to s3 bucket
     fs.readdirSync(buildDirectory).forEach((dirName) => {
       console.log(dirName);
       const fullPath = path.join(buildDirectory, dirName+`/lxp-cloud/dist/`);
@@ -61,7 +56,7 @@ export class LxpCdkStack extends cdk.Stack {
         destinationBucket: lxpBucket,
         destinationKeyPrefix: dirName,
       })
-      // cloud fornt diployment for the version 
+      // cloudfornt diployment for the version 
       const cloudfront = new cf.Distribution(this, cloud_front_name,{
         defaultRootObject : 'issueTreeModuleEntry.html',  // default page to load when accessing the website
         defaultBehavior: {
@@ -73,74 +68,14 @@ export class LxpCdkStack extends cdk.Stack {
       certificate : certifiate_lxpdev,
       domainNames: [`${dirName}.dev.lxp.optimizoryapps.com`],
     })
+    // map the domain name to the cloudfront distribution with alias record in route53
     new route53.ARecord(this, `AliasRecord_${dirName}`, {
       zone,
       recordName: dirName,
       target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cloudfront)),
     });
 
-    });        
-
-    // // s3 bucket deployment
-    // new BucketDeployment(this, 'LxpBucketDeployment', {
-    //   sources: [Source.asset('C:/Users/pulkit/Desktop/lxp/lxp-cloud/dist')], // have to replace with app data app_test is for testing
-    //   destinationBucket: lxpBucket,
-    //   destinationKeyPrefix: 'v1',
-    // });
-    // // origin access identity
-    // // const originAccessIdentity = new cf.OriginAccessIdentity(this, 'LxpOriginAccessIdentity', {
-    // //   comment: 'LxpOriginAccessIdentity',
-    // // });
-    // lxpBucket.grantRead(originAccessIdentity);
-    // const s3Origin_v1 = new S3Origin(lxpBucket, { originPath: `/v1`,originAccessIdentity },);
-
-    // // cloudfront distribution
-    // const distribution = new cf.Distribution(this, 'LxpDistribution',{
-    //   defaultRootObject : 'issueTreeModuleEntry.html',  // default page to load when accessing the website
-    //   defaultBehavior: {
-    //     origin: s3Origin_v1,
-    //     //origin: new S3Origin(lxpBucket,{originAccessIdentity}),
-    //     viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-    //     allowedMethods: cf.AllowedMethods.ALLOW_ALL,
-    //     //viewerProtocolPolicy: cf.ViewerProtocolPolicy.ALLOW_ALL,
-    //     cachedMethods: cf.CachedMethods.CACHE_GET_HEAD_OPTIONS,
-        
-    //   },
-    //   certificate : certifiate_lxpdev,
-    //   domainNames: ['v1.dev.lxp.optimizoryapps.com'],
-      
-    //   enabled:true,
-    //   });
-
-    //   const distribution_1 = new cf.Distribution(this, 'LxpDistribution_1',{
-    //     defaultRootObject : 'issueTreeModuleEntry.html',  // default page to load when accessing the website
-    //     defaultBehavior: {
-    //       origin: new S3Origin(lxpBucket,{originAccessIdentity}),
-    //       viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-    //       allowedMethods: cf.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-    //       //viewerProtocolPolicy: cf.ViewerProtocolPolicy.ALLOW_ALL,
-          
-    //     },
-    //     certificate : certifiate_lxpdev,
-    //     domainNames: ['v3.dev.lxp.optimizoryapps.com'],
-        
-    //     enabled:true,
-    //     });
-
-    
-    
-
-    // new route53.ARecord(this, 'AliasRecord', {
-    //   zone,
-    //   recordName: 'v1',
-    //   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
-    // });
-
-    // new route53.ARecord(this, 'AliasRecord_1', {
-    //   zone,
-    //   recordName: 'v3',
-    //   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution_1)),
-    // });
+    });
 
     }
 }
