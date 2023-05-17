4fa1d92147f49cb0dd89460094da637b532e673b
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2023-05-17 13:20:59 +0530

    tooltip container performance fix done

--- src/components/common/TooltipContainer.tsx
+++ src/components/common/TooltipContainer.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Tooltip from "@atlaskit/tooltip";
+
 export const TooltipContainer = ({
   content,
   position = "bottom-end",
   children,
-}) => {
-  return (
-    //@ts-ignore
-    <Tooltip content={content} position={position}>
-      {(props) => <div {...props}>{children}</div>}
-    </Tooltip>
-  );
+}): JSX.Element => {
+  return <span title={content}>{children}</span>;
 };
