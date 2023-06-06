f2c3bd6b1156e3878177780de363776c63d1d86d
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2023-06-06 15:59:17 +0530

    update-descriptor script is now parameterized

--- prebuild.sh
+++ prebuild.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+BASE_URL=${1:-https://lxp.optimizoryapps.com}
 
 echo "Removing old build directory and cache"
 rm -rf ./dist;
 rm -rf ./.cache;
-echo "Setting baseUrl to Amazon Cloudfront url https://lxp.optimizoryapps.com"
+echo "Setting baseUrl to Amazon Cloudfront url $BASE_URL"
 # jq '.baseUrl = "https://lxp.optimizoryapps.com" | .links.self = "https://lxp.optimizoryapps.com/atlassian-connect.json"' assets/original-atlassian-connect.json > assets/atlassian-connect.json
 
-node descriptorUpdater.js https://lxp.optimizoryapps.com
+node descriptorUpdater.js "$BASE_URL"
--- prebuild_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Removing old build directory and cache"
-rm -rf ./dist;
-rm -rf ./.cache;
-echo "Setting baseUrl to Amazon Cloudfront url https://lxp-dev.optimizoryapps.com"
-# jq '.baseUrl = "https://lxp-dev.optimizoryapps.com" | .links.self = "https://lxp-dev.optimizoryapps.com/atlassian-connect.json"' assets/original-atlassian-connect.json > assets/atlassian-connect.json
-
-node descriptorUpdater.js https://lxp-dev.optimizoryapps.com
