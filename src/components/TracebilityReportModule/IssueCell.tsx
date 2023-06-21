dcb0f5c2fb796563750c37913d9cf61f34ae0345
Anamika Soni <anamika@optimizory.com>
2023-06-21 21:00:16 +0530

    Added individual expand/ collapse for cell

--- src/components/TracebilityReportModule/IssueCell.tsx
+++ src/components/TracebilityReportModule/IssueCell.tsx
@@ -1,50 +1,59 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Button from "@atlaskit/button";
+import React, { useState, useEffect } from "react";
 import { MaxWidthContainer } from "./IssueTypeRow";
 import styled from "styled-components";
 import { displayAllIssueCardsId } from "../../constants/traceabilityReport";
+import ChevronDownIcon from "@atlaskit/icon/glyph/chevron-down";
+import ChevronUpIcon from "@atlaskit/icon/glyph/chevron-up";
 export interface Props {
   selectedSettingsDropdownIds: string[];
+  issueCards: JSX.Element[];
 }
 
 const ButtonContainer = styled.div`
   margin-top: 8px;
 `;
+
+const ExpandButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
 export const IssueCell = ({ selectedSettingsDropdownIds, issueCards }) => {
-  const [areAllIssuesVisible, setAreAllIssuesVisible] = useState(false);
-  let issueCardsToShow = [];
-  if (issueCards.length > 3) {
-    if (!areAllIssuesVisible) {
-      issueCardsToShow = issueCards.slice(0, 3);
-    } else {
-      issueCardsToShow = issueCards;
-    }
-  } else {
-    issueCardsToShow = issueCards;
-  }
-  const handleClick = () => {
-    if (issueCards.length > 3) {
-      setAreAllIssuesVisible(!areAllIssuesVisible);
-    }
-  };
-  selectedSettingsDropdownIds.forEach((issueId) => {
-    if (issueId == displayAllIssueCardsId) {
-      issueCardsToShow = issueCards;
-      return false;
-    }
-  });
+  const [isExpanded, setIsExpanded] = useState(false);
+
   useEffect(() => {
-    setAreAllIssuesVisible(false);
+    const isDisplayAllSelected = selectedSettingsDropdownIds.includes(
+      displayAllIssueCardsId
+    );
+    if (isDisplayAllSelected) {
+      setIsExpanded(true);
+    } else {
+      setIsExpanded(false);
+    }
   }, [selectedSettingsDropdownIds]);
+
+  const handleClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  let issueCardsToShow = issueCards;
+  if (issueCards.length > 3 && !isExpanded) {
+    issueCardsToShow = issueCards.slice(0, 3);
+  }
+
   return (
     <div>
       <MaxWidthContainer>{issueCardsToShow}</MaxWidthContainer>
-      {issueCards.length > issueCardsToShow.length && (
+      {issueCards.length > 3 && (
         <ButtonContainer>
-          <Button onClick={handleClick} spacing="compact">
-            More
-          </Button>
+          <ExpandButton onClick={handleClick}>
+            {isExpanded ? (
+              <ChevronUpIcon size="medium" label="Collapse" />
+            ) : (
+              <ChevronDownIcon size="medium" label="Expand" />
+            )}
+          </ExpandButton>
         </ButtonContainer>
       )}
     </div>
