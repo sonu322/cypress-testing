1a069bd2d85d57f6b41ded829e511464f03abd47
sonu322 <99172385+sonu322@users.noreply.github.com>
2023-09-21 12:26:26 +0530

    Update spec-lxpcloud.cy.js
    adding priority visibility

--- cypress/e2e/spec-lxpcloud.cy.js
+++ cypress/e2e/spec-lxpcloud.cy.js
@@ -1,35 +1,22 @@
 
-
 describe('Login to Atlassian Jira and Navigate to issue Page', () => {
 
   it('Login to Jira and navigate to the specified page', () => {
-  
+    cy.viewport(1200, 800);
     cy.visit('https://sonuk3.atlassian.net/login');
-      cy.viewport(1200, 800);
-
-    
-    cy.get('#username').type('sonum931@gmail.com');
+    cy.get('#username').should('be.visible').type('sonum931@gmail.com');
+    cy.get('#login-submit').click({ force: true });
+    cy.get('#password').should('be.visible').type('@tlassian@12');
     cy.get('#login-submit').click();
-     //cy.wait(5000);
-
-    cy.get('#password').type('@tlassian@12', { force: true });
-
-     cy.wait(10000);
-    cy.get('#login-submit').click();
-
     
-    cy.wait(20000); 
-    cy.visit('https://sonuk3.atlassian.net/browse/KAN-5');
+    cy.wait(15000); 
+    cy.visit('https://sonuk3.atlassian.net/browse/KAN-6')
     cy.wait(10000);
-
-    cy.origin('https://sonuk3.atlassian.net/browse/KAN-5', () => {
-       cy.wait(10000);
-    cy.contains("Links Explorer").click();
-     cy.wait(20000);
-
-
-    })
-    
-      
-  });
+    cy.origin('https://sonuk3.atlassian.net/browse/KAN-6', () => {
+      cy.contains("Links Explorer").click()
+      cy.wait(5000);
+      cy.get('iframe').its('0.contentDocument.body').should('be.visible').then(cy.wrap).contains("Priority").click()
+    });  
+    cy.wait(10000);
+    });
 });
