fb2d96ce65e22d20de63793fcd953ca87e85bd98
sonu322 <99172385+sonu322@users.noreply.github.com>
2023-09-25 21:07:01 +0530

    DC_issue_link_type_button

---
+++ cypress/e2e/issue_link_type.cy.js
@@ -0,0 +1,35 @@
+
+describe("Login to Jira", () => {
+  before(() => {
+    cy.login();
+    cy.wait(2000);
+  });
+
+  it("should interact with checkboxes and verify priority dropdown options", () => {
+    cy.visit("http://localhost:8080/browse/PM-1");
+    cy.contains("Links Explorer").click();
+
+    cy.wait(1000);
+   
+    cy.contains('Issue Link Type').click();
+  
+  cy.wait(1000);
+
+    cy.get('#Issue\\ Link\\ Type-options').should('be.visible'); 
+
+  
+cy.get('#PARENT').should('exist').click(); 
+
+cy.get('#CHILD_ISSUES').should('exist').click();
+
+   cy.wait(1000);
+
+cy.contains('span.css-178ag6o', 'Clear All').click();
+
+   cy.wait(1000);
+
+  });
+});
+
+
+
