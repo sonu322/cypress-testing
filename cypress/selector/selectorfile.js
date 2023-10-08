7b5e799fda594eaefd62df601c987181a5c5cfba
sonu322 <99172385+sonu322@users.noreply.github.com>
2023-10-08 22:36:30 +0530

    selector folder

---
+++ cypress/selector/selectorfile.js
@@ -0,0 +1,19 @@
+module.exports = {
+	userFieldName: "#login-form-username",
+	passwordFieldName: "#login-form-password",
+	loginButton: "#login-form-submit",
+	linksExplorer: "#link-issue-tab-panel",
+	prioritybutton: "div:contains('Priority')",
+	//priorityOptions: "#Priority-options",
+	priorityOptions: 'div#Priority-options button[role="checkbox"]',
+	issueLinkType:"div:contains('Issue Link Type')",
+	issueLinkTypeOptions: 'div#Issue\\ Link\\ Type-options button[role="checkbox"]',
+    issueType: "div:contains('Issue Type')",
+	issueCardFields: "div:contains('Issue Card Fields')",
+	// clearAll: '.span.css-178ag6o',
+	searchInput: 'div[data-ds--text-field--container="true"] input[placeholder="Search"]',
+	// Click on the "Export CSV" button
+    exportCSVButton: 'span[title="Click here to export CSV file"]',
+	// Selector for the "Get Help" link
+	getHelpLink: 'span[title="Get Help"] a.css-18q8r94',
+};
